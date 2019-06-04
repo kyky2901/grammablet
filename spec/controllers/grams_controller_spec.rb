394ba07af5345619432a9f5735e4d683b838515d
Kylie <kylie.rae.slattery@gmail.com>
2019-06-04 15:51:48 +0000

    added test for new action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe GramsController, type: :controller do
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
-      expect(response).to have_http_status(:success)
-      
-    end
-    
+      expect(response).to have_http_status(:success)     
+    end    
   end
+
+describe "grams#new action" do
+  it "should successfully show the new form" do
+    get :new
+    expect(response).to have_http_status(:success)
+  end
+end
+
 
 end
